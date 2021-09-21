961e4e138c9d4621131cccb6ce2b048d37ca6279
Khalid Saifullah <khalid@outlook.com.au>
2021-09-21 20:56:05 +1000

    test coverage for tx failure

--- tests/snowplow_tsv_to_json/test_main.py
+++ tests/snowplow_tsv_to_json/test_main.py
@@ -11,6 +11,7 @@ def test_handler():
     with open(os.path.join(__location__, 'event.json'), 'r') as event_file_handle, \
         open(os.path.join(__location__, 'transformed.txt'), 'r') as transformed_file_handle:
         lambda_response = lambda_handler(json.loads(event_file_handle.read()), None)
-        encoded_data = lambda_response.get('records')[0].get('data')
-        data = base64.b64decode(encoded_data).decode('utf-8')
-        assert data == transformed_file_handle.readline()
+        for record in lambda_response.get('records'):
+            encoded_data = record.get('data')
+            data = base64.b64decode(encoded_data).decode('utf-8')
+            assert data == transformed_file_handle.readline()
